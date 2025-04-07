0c24c1c3c80459a8eb0ea1958e357645e5af43b6
pfransozi <philipehf@gmail.com>
2025-04-06 22:48:50 -0300

    add aula01

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+
+// Etapa 1
+// Criação do grid para o jogo snake.
+
+// determina o tamanho de cada parte do grid
+let resolucao = 20;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(110);
+
+  //percorre a largura do canvas para criar as partes que compõem o grid
+  for (let x = 0; x < width; x += resolucao) {
+    //percorre a algura do canvas para criar as partes que compõem o grid
+    for (let y = 0; y < height; y += resolucao) {
+            stroke(200);
+      noFill();
+      //cria um retangulo
+      //rect(x, y, width, height)
+      rect(x, y, resolucao, resolucao);
+    }
+  }
 }
