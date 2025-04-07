9ed445a86fee10f3a1a1e924e21aa3d177713a72
pfransozi <philipehf@gmail.com>
2025-04-06 23:02:48 -0300

    add aula02

--- sketch.js
+++ sketch.js
@@ -1,26 +1,48 @@
 
-// Etapa 1
-// Criação do grid para o jogo snake.
+// Etapa 2
+// Criação de uma cobra movendo-se sem controles
 
 // determina o tamanho de cada parte do grid
 let resolucao = 20;
+let snake;
 
 function setup() {
   createCanvas(600, 400);
+  //modifica a quantidade de frames que são desenhados na função draw()
+  frameRate(5);
+
+  //define a cobra do jogo
+  snake = {
+    x:0, //posição inicial horizontal
+    y:0 //posição inicial vertical
+  };
 }
 
 function draw() {
-  background(110);
+  background(220);
 
-  //percorre a largura do canvas para criar as partes que compõem o grid
-  for (let x = 0; x < width; x += resolucao) {
-    //percorre a algura do canvas para criar as partes que compõem o grid
-    for (let y = 0; y < height; y += resolucao) {
-            stroke(200);
-      noFill();
-      //cria um retangulo
-      //rect(x, y, width, height)
-      rect(x, y, resolucao, resolucao);
-    }
-  }
+  //para ver o grid remova os comentários
+  // //percorre a largura do canvas para criar as partes que compõem o grid
+  // for (let x = 0; x < width; x += resolucao) {
+  //   //percorre a algura do canvas para criar as partes que compõem o grid
+  //   for (let y = 0; y < height; y += resolucao) {
+  //           stroke(200);
+  //     noFill();
+  //     //cria um retangulo
+  //     //rect(x, y, width, height)
+  //     rect(x, y, resolucao, resolucao);
+  //   }
+  // }
+
+  //modifica a posição x da cobra
+  snake.x += 1;
+  //modifica a posição y da cobra
+  snake.y += 1;
+
+  //muda a cor;
+  //fill(R, G, B)
+  fill(0, 255, 0);
+  //desenha a cobra baseada nos valoes de snake.x e snake.y e a resolucao;
+  //rect(x, y, width, height)
+  rect(snake.x * resolucao, snake.y * resolucao, resolucao, resolucao);
 }
